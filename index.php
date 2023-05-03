1f969475b69ea1fc30abebf92cd3e6c5cd4deacc
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-05-03 13:20:16 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="response.php" method="GET">
+    <label for="testo">testo</label>
+    <input type="text" id="testo" name="testo">
+    <label for="testoscurare">testo da oscurare</label>
+    <input type="text" id="testoscurare" name="testoscurare">
+    <button type="submit">Invia</button>
+</form>
+    
+</body>
+</html>
